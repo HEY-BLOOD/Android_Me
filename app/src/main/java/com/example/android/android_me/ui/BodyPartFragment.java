26220ba5eef015e905e3f97c6dd802118eb03df5
blood <hey-blood@foxmail.com>
2021-06-15 17:48:11 +0800

    点击响应和保存状态

--- app/src/main/java/com/example/android/android_me/ui/BodyPartFragment.java
+++ app/src/main/java/com/example/android/android_me/ui/BodyPartFragment.java
@@ -1,7 +1,6 @@
 package com.example.android.android_me.ui;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -10,8 +9,8 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 
 import com.example.android.android_me.R;
-import com.example.android.android_me.data.AndroidImageAssets;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class BodyPartFragment extends Fragment {
@@ -19,6 +18,10 @@ public class BodyPartFragment extends Fragment {
     private final static String TAG = BodyPartFragment.class.getSimpleName();
     private List<Integer> mImageIds;
     private int mListIndex;
+
+    // Final Strings to store state information about the list of images and list index
+    public static final String IMAGE_ID_LIST = "image_ids";
+    public static final String LIST_INDEX = "list_index";
 
     public void setImageIds(List<Integer> imageIds) {
         mImageIds = imageIds;
@@ -34,16 +37,54 @@ public class BodyPartFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Load the saved state (the list of images and list index) if there is one
+        if (savedInstanceState != null) {
+            mImageIds = savedInstanceState.getIntegerArrayList(IMAGE_ID_LIST);
+            mListIndex = savedInstanceState.getInt(LIST_INDEX);
+        }
+
+        // Inflate the Android-Me fragment layout
         View rootView = inflater.inflate(R.layout.fragment_body_part, container, false);
 
-        ImageView imageView = rootView.findViewById(R.id.body_part_image_view);
+        // Get a reference to the ImageView in the fragment layout
+        final ImageView imageView = (ImageView) rootView.findViewById(R.id.body_part_image_view);
 
+        // If a list of image ids exists, set the image resource to the correct item in that list
+        // Otherwise, create a Log statement that indicates that the list was not found
         if (mImageIds != null) {
+            // Set the image resource to the list item at the stored index
             imageView.setImageResource(mImageIds.get(mListIndex));
-        }else{
-            Log.v(TAG, "This fragment has null list of image id's");
+
+            // Set a click listener on the image view
+            imageView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    // Increment position as long as the index remains <= the size of the image ids list
+                    if (mListIndex < mImageIds.size() - 1) {
+                        mListIndex++;
+                    } else {
+                        // The end of list has been reached, so return to beginning index
+                        mListIndex = 0;
+                    }
+                    // Set the image resource to the new list item
+                    imageView.setImageResource(mImageIds.get(mListIndex));
+                }
+            });
+        } else {
+            Log.v(TAG, "This fragment has a null list of image id's");
         }
 
         return rootView;
+    }
+
+    /**
+     * Save the current state of this fragment
+     */
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        outState.putIntegerArrayList(IMAGE_ID_LIST, (ArrayList<Integer>) mImageIds);
+        outState.putInt(LIST_INDEX, mListIndex);
+
+        super.onSaveInstanceState(outState);
     }
 }

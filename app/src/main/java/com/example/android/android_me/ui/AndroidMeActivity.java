3a8d35a37cc7c41d96c9955b0e81a9ed7146ccc5
blood <hey-blood@foxmail.com>
2021-06-16 10:53:21 +0800

    在 Fragment 之间通信

--- app/src/main/java/com/example/android/android_me/ui/AndroidMeActivity.java
+++ app/src/main/java/com/example/android/android_me/ui/AndroidMeActivity.java
@@ -41,6 +41,12 @@ public class AndroidMeActivity extends AppCompatActivity {
             // Set the list of image id's for the head fragment and set the position to the second image in the list
             headFragment.setImageIds(AndroidImageAssets.getHeads());
             headFragment.setListIndex(1);
+
+            // Get the correct index to access in the array of head images from the intent
+            // Set the default value to 0
+            int headIndex = getIntent().getIntExtra("headIndex", 0);
+            headFragment.setListIndex(headIndex);
+
             fragmentManager.beginTransaction()
                     .add(R.id.head_container, headFragment)
                     .commit();
@@ -48,6 +54,8 @@ public class AndroidMeActivity extends AppCompatActivity {
             // Create and display the body BodyPartFragment
             BodyPartFragment bodyFragment = new BodyPartFragment();
             bodyFragment.setImageIds(AndroidImageAssets.getBodies());
+            int bodyIndex = getIntent().getIntExtra("bodyIndex", 0);
+            bodyFragment.setListIndex(bodyIndex);
             fragmentManager.beginTransaction()
                     .add(R.id.body_container, bodyFragment)
                     .commit();
@@ -55,6 +63,8 @@ public class AndroidMeActivity extends AppCompatActivity {
             // Create and display the leg BodyPartFragment
             BodyPartFragment legFragment = new BodyPartFragment();
             legFragment.setImageIds(AndroidImageAssets.getLegs());
+            int legIndex = getIntent().getIntExtra("legIndex", 0);
+            legFragment.setListIndex(legIndex);
             fragmentManager.beginTransaction()
                     .add(R.id.leg_container, legFragment)
                     .commit();
